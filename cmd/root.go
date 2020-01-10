28019cdd235939397c9c423286aed60ade6d483d
Quentin Barrand <quentin@quba.fr>
2020-01-10 17:30:33 +0100

    Add a basic action.

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,30 @@
 package cmd
 
-import "github.com/urfave/cli/v2"
+import (
+	"github.com/urfave/cli/v2"
+)
 
 func CLI() *cli.App {
+	const name = "debuggo"
+
 	app := cli.NewApp()
+
+	app.Name = name
 	app.Version = "0.0.0"
 	app.Authors = []*cli.Author{
 		{Name: "Quentin Barrand", Email: "quentin@quba.fr"},
 		{Name: "Adam Krajewski"},
+	}
+
+	app.UsageText = name + " PATH [ARGS...]"
+
+	app.Action = func(c *cli.Context) error {
+		if c.NArg() < 1 {
+			// at least one path must be specified
+			return cli.ShowAppHelp(c)
+		}
+
+		return nil
 	}
 
 	return app
